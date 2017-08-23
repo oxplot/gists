25bc2980e4e1a238e7d7dabef67b335e6c18025a
Mansour Behabadi <mansour@oxplot.com>
2017-08-23 11:38:04 +1000

    Include qpdf in decrown process

--- decrown.py
+++ decrown.py
@@ -2,9 +2,18 @@
 # Copyright (C) 2016 Mansour Behabadi <mansour@oxplot.com>
 
 import re
+import subprocess
 import sys
+import tempfile
 
-data = sys.stdin.buffer.read()
+tmpfile = tempfile.NamedTemporaryFile()
+tmpfile.write(sys.stdin.buffer.read())
+tmpfile.flush()
+data, _ = subprocess.Popen(
+  ['qpdf', '--qdf', tmpfile.name, '-'],
+  stdout=subprocess.PIPE,
+).communicate()
+
 def sub(p):
   global data
   data = re.sub(p, b'', data, 1, re.M)
@@ -13,4 +22,11 @@ sub(br'^%%[^%]*?/Producer\s*\(\x50\x72\x69\x6e\x63\x65[^%]*?endobj$\n')
 sub(br'^%%[^%]*?/Contents\s*\(This document[^%]*?endobj$\n')
 sub(br'^%%[^%]*?0\.2314 0\.0471 0\.3765[^%]*?endobj$\n')
 
-sys.stdout.buffer.write(data)
+tmpfile.truncate(0)
+tmpfile.seek(0)
+tmpfile.write(data)
+tmpfile.flush()
+subprocess.call([
+  'qpdf', '--stream-data=compress', '--object-streams=generate',
+  tmpfile.name, '-',
+])
