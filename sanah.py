f1414bb44f32a78a54d2a252cc1ce5bc26233383
Mansour Behabadi <mansour@oxplot.com>
2022-11-21 15:28:12 +1100

    Remove by in copyright notice

--- sanah.py
+++ sanah.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # sanah - Strictly Non-Human random password generator
 #
-# Copyright (C) 2022 by Mansour Behabadi <mansour@oxplot.com>
+# Copyright (C) 2022 Mansour Behabadi <mansour@oxplot.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
